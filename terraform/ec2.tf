7abf3d06efc67ed00005e0878b0f430a19e785a7
yojan1226 <yojansawdekar127@gmail.com>
2025-11-22 14:19:31 +0530

    modified

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,11 @@
 resource "aws_instance" "node_server" {
-  
-  ami = "ami-0d176f79571d18a8f"
-  instance_type = "t2.xlarge"
-  key_name = aws_key_pair.ec2_key.key_name
+
+  ami                         = "ami-0d176f79571d18a8f"
+  instance_type               = "t2.xlarge"
+  key_name                    = "awskey"   # existing AWS keypair name
   associate_public_ip_address = true
 
   tags = {
     Name = "Nodeserver"
   }
-}
-
-resource "aws_key_pair" "ec2_key" {
-    key_name = "awskey"
-    public_key = file("${path.module}/keys/awskey.pub")
-
-  
 }
